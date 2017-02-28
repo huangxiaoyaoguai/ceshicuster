ecb682f78ee58b02fcf7f4664cf88c5a2eb94edc
huangcheng <921365631@qq.com>
2017-02-28 15:20:35 +0800

    xiugai

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ var cluster = require ( 'cluster' ) ;
 
 var numCPUs = require ( 'os' ) . cpus ( ) . length
 
-console.log(numCPUs);
+var spawn =require('child_process').spawn;
+
+
+
+// console.log(numCPUs);
 // console.log(cluster)
 if (cluster.isMaster) {
 	for (var i = 0; i < numCPUs; i++) {
@@ -20,15 +24,48 @@ if (cluster.isMaster) {
 
 
 
-app.all('/',function(req,res){
+		app.all('/',function(req,res){
 
-	res.sendfile('./index.html')
-})
+			res.sendfile('./index.html')
+		})
+
+
+		app.listen(3000,function(){
+			console.log('http://localhost:3000')
+
+		})
+
+
+		var span = spawn('webpack',['--watch']);
+		span.stdout.on('data',function(data){
+			var datastring = data.toString();
+			console.log(datastring)
+		})
+
+		span.on('exit',function(code,signal){
+			console.log('jieshu')
+
+		})
+		console.log(span.pid);
+
+		// console.log(process.pid,'1')
+		// console.log(process.execPath,'2')
+		// console.log(process.title,'3')
+
+		// process.stdin.resume();
+		// process.stdin.on('data',function(chunk){
+		// 	process.stdout.write('进程接受数据:'+chunk)
+		// })
+
+
+		// process.memoryUsage();
 
 
 
-app.listen(3000,function(){
-	console.log('http://localhost:3000')
-
-})
 }
+
+
+// process.stdin.resume();
+// 		process.stdin.on('data',function(chunk){
+// 			process.stdout.write('进程接受数据:'+chunk)
+// 		})
